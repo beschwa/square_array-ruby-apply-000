6dbbefe8ea541e6616e382e79c35125b58389912
Michael Naraiswar <naraiswar@gmail.com>
2019-02-04 01:28:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,6 @@
 def square_array(array)
-  array.each do |item|
-    array[array.index(item)] = item**2
-  end
+#  array.each do |item|
+#    array[array.index(item)] = item**2
+#  end
+  array.collect { |item| item**2 }
 end
